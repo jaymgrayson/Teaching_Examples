72c0f1b1d3800ab9eae85ef3db5dee66d56d61c8
Jason M Grayson <jaymgrayson@gmail.com>
2021-04-14 15:27:47 -0400

    Add files via upload

--- Deep_Dive_Script_1.R
+++ Deep_Dive_Script_1.R
@@ -1,7 +1,18 @@
 library(Seurat)
 library(SeuratData)
 library(SeuratDisk)
-setwd("~/Desktop/Deep_Dive/Farber_scmrna_data")
-Convert("COV022A1.h5ad",dest="h5seurat",overwrite = TRUE)
-COVID_1<-LoadH5Seurat("COV022A1.h5seurat")
-COVID_1
+setwd("~/Desktop/Deep_Dive/Modified_files")
+rm(list=ls())
+#Convert("COV022A1.h5ad",dest="h5seurat",overwrite = TRUE)
+
+list.filenames <- list.files(pattern=".h5ad$")
+print(list.filenames)
+list.data<-list()
+for (i in seq_along(list.filenames))
+{
+    name<-list.filenames[i]
+    Convert(name,dest="h5seurat",overwrite = TRUE)   
+}
+H5seurat_files<-list.files(pattern=".h5seurat$")
+print(H5seurat_files)
+
